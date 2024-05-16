a62bc7ddfe96ed9ecc6a22b767527c98e0d5ce30
Mulder Oosthuizen <muldero@pepkorit.com>
2024-05-16 10:18:08 +0200

    Initial Commit
    Setup the repo and started the project

---
+++ Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Capstone_Project
+{
+    internal class Constants
+    {
+        private static readonly string URL = @"https://demowebshop.tricentis.com/";
+        private static readonly string USERNAME = @"reg@reg.com";
+        private static readonly string PASSWORD = @"TESTING";
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Capstone_Project
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
